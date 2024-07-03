de907a0ca57f46d5305ce51d0c521dd8529989df
Paolo <paolo.vismara@certimeter.it>
2024-07-03 18:50:16 +0200

    main program

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    std::cout << "argumengts n: " << argc << "\n";
+    for(int i = 0; i < argc; i++) {
+        std::cout << argv[i] << "\n";
+    }
+    
+    std::cout << std::endl;
+
+    return 0;
+}
