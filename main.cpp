fb8a31a8d5ee5fa3347d2046ce05d30be310e9db
paolo <paolo@localhost.localdomain>
2024-07-31 10:34:41 +0200

    dev build

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 int main(int argc, char** argv)
 {
+    std::cout << "dev build\n";
     std::cout << "argumengts n: " << argc << "\n";
     for(int i = 0; i < argc; i++) {
         std::cout << argv[i] << "\n";
