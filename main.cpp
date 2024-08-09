7d7013693a6e277eb33c5068a7192fd9fa28d246
Paolo <paolo.vismara@certimeter.it>
2024-08-09 11:29:51 +0200

    fix

--- main.cpp
+++ main.cpp
@@ -2,11 +2,8 @@
 
 int main(int argc, char** argv)
 {
-<<<<<<< HEAD
     std::cout << "main build\n";
-=======
-    std::cout << "dev build\n";
->>>>>>> dev
+
     std::cout << "argumengts n: " << argc << "\n";
     for(int i = 0; i < argc; i++) {
         std::cout << argv[i] << "\n";
